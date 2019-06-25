43eb7305d42445218008d6a68dce474ad576354a
Clemens Holzkorn <c.holzkorn@hotmail.com>
2019-06-25 18:14:48 +0200

    basic methods working

--- email-administration/email-administration-app/src/emailapp/Email.java
+++ email-administration/email-administration-app/src/emailapp/Email.java
@@ -7,8 +7,11 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
-	private int mailboxCapacity;
+	private String email;
+	private int mailboxCapacity = 500;
+	private int defaultPasswordLength = 10;
 	private String alternateEmail;
+	private String companySuffix = "company.com";
 	
 	// Constructor to receive the first name and the last name
 	public Email(String firstName, String lastName) {
@@ -18,12 +21,22 @@ public class Email {
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
+		
+		// Call a method that returns a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
+		
+		// Combine elements to generate email
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+		System.out.println("Your email is: " + email);
+		
 	};
 	
 	
 	// Ask for the department
 	private String setDepartment() {
-		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		System.out.println("DEPARTMENT CODES: \n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) { return "sales"; }
@@ -34,10 +47,34 @@ public class Email {
 	
 	
 	// Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTVWXYZ0123456789!?#";
+		char[] password = new char[length];
+		for (int i=0; i<length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		};
+		return new String(password);
+	};
 	
 	// Set the mailbox capacity
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	};
 	
 	// Set the alternate email
+	public void setAlternateEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	};
 	
 	// Change the password
+	public void changePassword(String password) {
+		this.password = password;
+	};
+	
+	// Get methods
+	public int getMailboxCapacity() { return mailboxCapacity; };
+	public String getAlternateEmail() { return alternateEmail; };
+	public String getPassword() { System.out.print(this.password); return password; }
+	
 }
--- email-administration/email-administration-app/src/emailapp/EmailApp.java
+++ email-administration/email-administration-app/src/emailapp/EmailApp.java
@@ -5,6 +5,8 @@ public class EmailApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Email em1 = new Email("John", "Smith");
+		
+		em1.changePassword("randomstuff");
 	}
 
 }
